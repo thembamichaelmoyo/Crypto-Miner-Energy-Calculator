64d6928bad6b70b3f218734ab12f98786104cba4
Themba Michael Moyo <39517597+thembamichaelmoyo@users.noreply.github.com>
2022-06-27 16:28:20 +0200

    Code clean up.

--- miner.py
+++ miner.py
@@ -2,14 +2,14 @@ from hashlib import sha256
 import random
 import locale
 
-nonce_limit = 9999999999999999999
 previous_hash = input("What is the previous hash?\n")
 unit_price = int(input("What is your unit price in cents, per KWh?\n"))
-device_pwr = float(input("What is the maximum power rating in Watts?\n"))
-difficulty = random.randint(6, 8)
+device_pwr = float(input("What is your device's maximum power rating in Watts?\n"))
+prefix_zeros = random.randint(4, 6)
+block_number = random.randint(1, 999999999999999)
+nonce_limit = 9999999999999999999
 locale.setlocale(locale.LC_ALL, ("", "utf-8"))
 currency_symbol = locale.localeconv()["int_curr_symbol"]
-block_number = random.randint(1, 999999999999999)
 
 
 def SHA256(text):
@@ -36,9 +36,9 @@ if __name__ == "__main__":
 
     start = time.time()
     print("Coining in progress...")
-    new_hash = coining(block_number, transactions, previous_hash, difficulty)
+    new_hash = coining(block_number, transactions, previous_hash, prefix_zeros)
     duration = float((time.time() - start))
-    pwr_cons = float((duration / 360000) * device_pwr)
+    pwr_cons = float((duration / 3600000) * device_pwr)
     pwr_cost = float(pwr_cons) * unit_price
     print(f"Coining on block {block_number}, lasted {duration} seconds.")
     print(f"Coining power usage was {pwr_cons} Watts.")
